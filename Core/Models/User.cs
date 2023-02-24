eb5d8ef714e4af0c15e084fa86411e9a96c660d1
aussiearef <arefkr@gmail.com>
2023-02-25 10:52:04 +1100

    Added Code

---
+++ Core/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Models;
+
+public class User
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ Core/Services/LocalFileStorage.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.Services;
+
+public class LocalFileStorage : IFileStorage
+{
+    private readonly IHostingEnvironment _env;
+
+    public LocalFileStorage(IHostingEnvironment env)
+    {
+        _env = env;
+    }
+
+    public async Task StoreFile(IFormFile file, string key)
+    {
+        const string rootPath = "PhotoStore";
+
+        var wwwRootFolder = _env.WebRootPath;
+        var userFolder = Path.Combine(wwwRootFolder, rootPath);
+        var folder = Path.Combine(userFolder, key);
+
+        if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
+
+        var fullFilePath = Path.Combine(folder, Path.GetFileName(file.FileName) ?? string.Empty);
+
+        await using var targetStream = new FileStream(fullFilePath, FileMode.Create);
+        await file.CopyToAsync(targetStream);
+        targetStream.Close();
+    }
+}
